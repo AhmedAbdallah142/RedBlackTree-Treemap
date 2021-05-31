ee51caa9604dadc9d6f62474a4ba03fcd3d82950
Youssef <Youssef@192.168.1.5>
2021-05-31 08:21:29 +0200

    deletion ended

--- RedBlackTree & TreeMap/src/eg/edu/alexu/csd/filestructure/redblacktree/implement/RedBlackTree.java
+++ RedBlackTree & TreeMap/src/eg/edu/alexu/csd/filestructure/redblacktree/implement/RedBlackTree.java
@@ -1,5 +1,8 @@
 package eg.edu.alexu.csd.filestructure.redblacktree.implement;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import eg.edu.alexu.csd.filestructure.redblacktree.INode;
 import eg.edu.alexu.csd.filestructure.redblacktree.IRedBlackTree;
 
@@ -121,9 +124,186 @@ public class RedBlackTree<T extends Comparable<T>, V> implements IRedBlackTree<T
 
 	@Override
 	public boolean delete(T key) {
-		return false;
+		return deletNode(root,key);
 	}
 	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private boolean deletNode(INode<T, V> node, T key) {
+		// TODO Auto-generated method stub
+		INode z=nill;
+		INode x,y;
+		// we need first to get the node.
+		while(node!=nill) {
+			if(key.compareTo(node.getKey())==0) {
+				z=node;
+			}
+			if(key.compareTo(node.getKey())<0) {
+				node=node.getLeftChild();
+			}
+			else {
+				node=node.getRightChild();
+			}
+		}
+		if(z==nill) {								//if not found print message.
+			System.out.println("Couldn't find key in the tree");
+		    return false;
+		}
+		
+		y=z;
+		boolean yOriginalColor=y.getColor();
+		if(z.getLeftChild()==nill) {				// if it has no left children then replace z by the right child.
+			x=z.getRightChild();
+			rbTransplant(z, z.getRightChild());
+		}
+		else if(z.getRightChild()==nill) { 			// if it has no right children then replace z by the left child.
+			x=z.getLeftChild();
+			rbTransplant(z, z.getLeftChild());
+		}
+		else {										// in case it have right and left child
+			y = minimum(z.getRightChild());			// get the minimum child in right branch.
+			yOriginalColor=y.getColor();
+			x=y.getRightChild();
+			if(y.getParent()==z) {					// in case that there is 1 node in the right subtree
+				x.setParent(y);
+			}
+			else {									// in case of there is multiple node in the right subtree
+				/*
+				 * first fix the pointers between nill>>x<< node and the parent of y
+				 * becouse we move y to take the place of z.
+				 * x always is nill node.  
+				 */
+				rbTransplant(y, y.getRightChild());	
+				y.setRightChild(z.getRightChild());
+				y.getRightChild().setParent(y);
+			}
+			/*
+			 * fix the pointers between y node and the parent of z & all child of z.
+			 */
+			rbTransplant(z, y);
+			y.setLeftChild(z.getLeftChild());
+			y.getLeftChild().setParent(y);
+			y.setColor(z.getColor());
+		}
+		/*
+		 * this to fix the balanced of the tree in case of the deletion node is't red
+		 */
+		if(!yOriginalColor) {		// may occur error.
+			fixDelete(x);
+		}
+		return true;
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private void fixDelete(INode x) {
+		// TODO Auto-generated method stub
+		INode s;
+		while((x!=root) && (!x.getColor())) {
+			if(x==x.getParent().getLeftChild()) {		// case that x is left child.
+				s=x.getParent().getRightChild();		// s is the sibling of x.
+				if(s.getColor()) {						// if s is red then case 3 occur.
+					s.setColor(false);					// recolor s & x.
+					x.getParent().setColor(true);		
+					left_rotate(x.getParent());			// left rotate.
+					s=x.getParent().getRightChild();	// change pointer of s after the rotate.
+				}
+				/*
+				 * now we may be in case 1 or 2 
+				 */
+				if((!s.getLeftChild().getColor())&&(!s.getRightChild().getColor())) {	 
+					/*
+					 * if the 2 child of s is black
+					 * we in case 2 S is black and both its children are black
+					 */
+					s.setColor(true);		// recolor s				
+					x=x.getParent();		
+				}
+				else {
+					/*
+					 * if at least 1 is balck 
+					 * if the right child of s is the black node then enter if condition
+					 */
+					if(!s.getRightChild().getColor()) {
+						/*
+						 * case 2 right left. 
+						 */
+						s.getLeftChild().setColor(false);
+						s.setColor(true);
+						right_rotate(s);
+						s=x.getParent().getRightChild();
+					}
+					
+					/*
+					 * case 2 right right. 
+					 */
+					s.setColor(x.getParent().getColor());	
+					x.getParent().setColor(false);
+					s.getRightChild().setColor(false);
+					left_rotate(x.getParent());
+					x=root;
+				}
+				
+			}
+			/*
+			 * case that x is right child.
+			 * same as the previous iteration but swap from left to right.
+			 */
+			else {
+				s=x.getParent().getLeftChild();
+				if(s.getColor()) {
+					s.setColor(false);
+					x.getParent().setColor(true);
+					right_rotate(x.getParent());
+					s=x.getParent().getLeftChild();
+				}
+				if((!s.getLeftChild().getColor())&&(!s.getRightChild().getColor())) {
+					s.setColor(true);
+					x=x.getParent();
+				}
+				else {
+					if(!s.getLeftChild().getColor()) {
+						s.getRightChild().setColor(false);
+						s.setColor(true);
+						left_rotate(s);
+						s=x.getParent().getLeftChild();
+					}
+					
+					s.setColor(x.getParent().getColor());
+					x.getParent().setColor(false);
+					s.getLeftChild().setColor(false);
+					right_rotate(x.getParent());
+					x=root;
+				}
+			}
+		}
+		x.setColor(false);
+	}
+	/*	
+	 * to get the minimum element in the right subtree. 
+	 */
+	@SuppressWarnings("rawtypes")
+	private INode minimum(INode node) {
+		// TODO Auto-generated method stub
+		 while (node.getLeftChild() != nill) {
+		      node = node.getLeftChild();
+		 }
+		 return node;
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private void rbTransplant(INode u, INode v) {
+		// TODO Auto-generated method stub
+		if(u.getParent()==null) {						// in case that we delete the root.
+			root=v;
+		}
+		else if(u==u.getParent().getLeftChild()) {		//in case that u is the left child of the parent change pointer.
+			u.getParent().setLeftChild(v);
+		}
+		else {											//in case that u is the right child of the parent change pointer.
+			u.getParent().setRightChild(v);			
+		}
+		v.setParent(u.getParent());						// fix the pointer of the new child to the parent of u.
+	}
+
 	private void right_rotate(INode<T, V> x) {
 		INode<T, V> y = x.getLeftChild();
 		x.setLeftChild(y.getRightChild());
@@ -157,20 +337,71 @@ public class RedBlackTree<T extends Comparable<T>, V> implements IRedBlackTree<T
 	
 	public static void main(String[] args) {
 		RedBlackTree<Integer, Integer> t = new RedBlackTree<Integer, Integer>();
-		t.insert(41, -1);
+		/*t.insert(41, -1);
 		t.insert(38, -1);
 		t.insert(31, -1);
 		t.insert(12, -1);
 		t.insert(19, -1);
 		t.insert(8, -1);
-		t.dfs_left(t.root);
+		t.dfs_left(t.root);*/
+		t.insert(5,-1);
+		t.insert(3,-1);
+		t.insert(8,-1);
+		t.insert(7,-1);
+		t.insert(10,-1);
+		t.insert(9,-1);
+		printLevelOrder(t.getRoot());
+		System.out.println("\n delete");
+		t.delete(8);
+		printLevelOrder(t.getRoot());
 	}
-	private void dfs_left(INode<T, V> root) {
+	@SuppressWarnings("rawtypes")
+	static void printLevelOrder(INode root)
+    {
+        // Base Case
+        if(root == null)
+            return;
+         
+        // Create an empty queue for level order traversal
+        Queue<INode> q =new LinkedList<INode>();
+         
+        // Enqueue Root and initialize height
+        q.add(root);
+         
+         
+        while(true)
+        {
+             
+            // nodeCount (queue size) indicates number of nodes
+            // at current level.
+            int nodeCount = q.size();
+            if(nodeCount == 0)
+                break;
+             
+            // Dequeue all nodes of current level and Enqueue all
+            // nodes of next level
+            while(nodeCount > 0)
+            {
+            	INode node = q.peek();
+                System.out.print(node.toString());
+                q.remove();
+                if(node.getLeftChild() != null)
+                    q.add(node.getLeftChild());
+                if(node.getRightChild() != null)
+                    q.add(node.getRightChild());
+                nodeCount--;
+            }
+            System.out.println();
+        }
+    }
+	
+	
+	/*private void dfs_left(INode<T, V> root) {
 		if(root == nill) return;
 		System.out.println(root.toString());
 		dfs_left(root.getLeftChild());
 		dfs_left(root.getRightChild());
-	}
+	}*/
 	
 
 }
