c9ac0650ddee73d34efadd790c52c8880b42e09b
ahmed <ahmed@192.168.1.6>
2021-06-01 09:41:34 +0200

    floor Entry

--- RedBlackTree & TreeMap/src/eg/edu/alexu/csd/filestructure/redblacktree/implement/TreeMap.java
+++ RedBlackTree & TreeMap/src/eg/edu/alexu/csd/filestructure/redblacktree/implement/TreeMap.java
@@ -10,6 +10,14 @@ import eg.edu.alexu.csd.filestructure.redblacktree.ITreeMap;
 
 public class TreeMap<T extends Comparable<T>, V> implements ITreeMap{
 	
+	RedBlackTree<T, V> tree;
+	int treeSize;
+	public TreeMap() {
+		tree = new RedBlackTree<>();
+		treeSize = 0;
+	}
+	
+	
 	@Override
 	public Entry ceilingEntry(Comparable key) {
 		// TODO Auto-generated method stub
@@ -58,9 +66,29 @@ public class TreeMap<T extends Comparable<T>, V> implements ITreeMap{
 		return null;
 	}
 
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
 	public Entry floorEntry(Comparable key) {
-		// TODO Auto-generated method stub
+		Node temp = (Node) tree.getRoot();
+		Node Parent =  null;
+		while (temp != null) {
+			if (temp.getKey().compareTo(key)>0) {
+				if ((temp.getLeftChild()==null)&&(Parent!=null)) {
+					return (Entry) Parent;
+				}
+				temp = (Node) temp.getLeftChild();
+			}
+			else if (temp.getKey().compareTo(key) == 0) {
+				return (Entry) temp;
+			}
+			else {
+				if (temp.getRightChild()==null) {
+					return (Entry) temp;
+				}	
+				Parent = temp;
+				temp = (Node) temp.getRightChild();	
+			}
+		}
 		return null;
 	}
 
